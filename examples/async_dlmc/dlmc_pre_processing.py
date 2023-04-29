a5e862497107bc9437b4008d7792c9dad4e6114f
JordanHa19 <jordan_harrington@live.com>
2023-04-29 15:48:51 -0400

    py format number 3

--- examples/async_dlmc/dlmc_pre_processing.py
+++ examples/async_dlmc/dlmc_pre_processing.py
@@ -6,14 +6,6 @@ from scipy.io import mmread
 from scipy.io import mmwrite
 from io import BytesIO
 import sys
-from scipy.io.mmio import MMFile
-
-
-class MMFileFixedFormat(MMFile):
-    def _field_template(self, field, precision):
-        # Override MMFile._field_template.
-        return f'%.{precision}f\n'
-
 
 def generate_dist_matrix(theta, sigma, network_size):
     return np.array([np.random.normal(loc = theta, scale = sigma, size = 100) for x in range(network_size)])
@@ -22,9 +14,8 @@ theta = float(sys.argv[1])
 sigma = float(sys.argv[2])
 network_size = int(sys.argv[3])
 system_hold_folder = sys.argv[4]
-
 np.set_printoptions(suppress=True)
-
+dir = Path('.')
 distributions = generate_dist_matrix(theta, sigma, network_size)
 mmwrite(system_hold_folder + "dist_matrix.mtx", distributions)
 
